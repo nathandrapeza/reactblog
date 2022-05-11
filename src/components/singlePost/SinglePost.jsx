a78656abcaa735b4baaa1cf05cea56c906295397
nathandrapeza <drapeza@ualberta.ca>
2022-05-11 12:26:09 -0600

    singlePost page complete

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,32 @@
+import './singlepost.css'
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+				<img src="https://images.pexels.com/photos/731022/pexels-photo-731022.jpeg"
+				alt="Dog sleeping on bed."
+				className="singlePostImage"
+				/>
+				<h1 className="singlePostTitle">
+					Lorem ipsum dolor sit amet.
+					<div className="singlePostEdit">
+						<i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+						<i className="singlePostIcon fa-solid fa-trash-can"></i>
+					</div>
+				</h1>
+				<div className="singlePostInfo">
+					<span className="singlePostAuthor">Author: <b>Nathan</b></span>
+					<span className="singlePostDate">1 hour ago</span>
+				</div>
+				<p className="singlePostDesc">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+    </div>
+  )
+}
