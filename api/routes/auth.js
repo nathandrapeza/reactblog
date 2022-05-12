4fc02a705aba71b1b44a07568449690362dbf8e2
nathandrapeza <drapeza@ualberta.ca>
2022-05-12 14:02:16 -0600

    Added api method for user login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User"); // grab user user model
-
+const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req,res)=> {
     try {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPass,
         });
         const user = await newUser.save(); //.save() is from mongoose
         res.status(200).json(user);
@@ -18,5 +21,20 @@ router.post("/register", async (req,res)=> {
 })
 
 //LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Wrong credentials!");
+    
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong credentials!");
+
+        const {password, ...others} = user._doc; // prevent password from being sent back
+        res.status(200).json(others)
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
