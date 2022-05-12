8d8b3bffe04f56d4abec1992b1e233648b1c6b0d
nathandrapeza <drapeza@ualberta.ca>
2022-05-11 23:24:50 -0600

    routing complete. front-end display complete but not yet interactive or functional.

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import './topBar.css'
 
 export default function TopBar() {
+  const user = true;
   return (
     <div className="top">
 			<div className="topLeft">
@@ -11,18 +13,41 @@ export default function TopBar() {
 			</div>
 			<div className="topCenter">
 				<ul className="topList">
-					<li className="topListItem">HOME</li>
-					<li className="topListItem">ABOUT</li>
-					<li className="topListItem">CONTACT</li>
-					<li className="topListItem">WRITE</li>
-					<li className="topListItem">LOGOUT</li>
+					<li className="topListItem">
+						<Link to="/" className="link">HOME</Link>
+					</li>
+					<li className="topListItem">
+						<Link to="/" className="link">ABOUT</Link>
+					</li>
+					<li className="topListItem">
+						<Link to="/" className="link">CONTACT</Link>
+					</li>
+					<li className="topListItem">
+						<Link to="/write" className="link">WRITE</Link>
+					</li>
+					<li className="topListItem">
+						{user && "LOGOUT"}
+					</li>
 				</ul>
 			</div>
 			<div className="topRight">
+				{
+					user ? (
+				
 				<img className="topImg"
 				  src="https://media-exp1.licdn.com/dms/image/C5603AQEH9A8NCagiWw/profile-displayphoto-shrink_800_800/0/1652123095601?e=1657756800&v=beta&t=0TgwPIsHoFuHP6YsIjBjKWvro-N-KKV9btnoiyqCat8"
 					alt="Nathan Drapeza standing next to a Camel.">
 				</img>
+					) : (
+						<ul className="topList">
+							<li className="topListItem">
+								<Link to="/login" className="link">LOGIN</Link>
+							</li>
+							<li className="topListItem">
+								<Link to="/register" className="link">REGISTER</Link>
+							</li>
+						</ul>
+					)} 
 				<i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
 			</div>
     </div>
