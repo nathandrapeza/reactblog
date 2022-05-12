7fa10cbcbd3daffd77d6452d51cac248c979ca1f
nathandrapeza <drapeza@ualberta.ca>
2022-05-12 12:53:22 -0600

    Added api method for new user registration

--- api/index.js
+++ api/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
+
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(console.log("CONNECTED TO MONGODB")).catch(err=>console.log(err));
+
+app.use("/api/auth", authRoute);
+
+
 
 /*
 app.use("/", (req,res) => {
